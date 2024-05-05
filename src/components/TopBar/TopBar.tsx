95a9592fe2d6284dfc1227149d32ee8d5c9620cc
CDamianS <A01656277@tec.mx>
2024-05-05 14:13:53 -0600

    Final aesthetic changes

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,26 +1,34 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import cuevana from '../../constants/cuevana.png'
-import { ROUTES } from '../../routes/constants'
+import React from "react";
+import { Link } from "react-router-dom";
+// import cuevana from "../../constants/cuevana.png";
+import { ROUTES } from "../../routes/constants";
 
 const TopBar = () => {
     return (
-        <div className='bg-ctp-base flex font-sans font-semibold items-center justify-between mx-auto p-4 text-ctp-blue'>
-            <Link to={ROUTES.HOME} className='text-2xl'>
-                <img src={cuevana} alt="" />
-
+        <div className="bg-ctp-base flex font-sans font-semibold items-center justify-between mx-auto p-4 text-ctp-blue">
+            <Link to={ROUTES.HOME} className="text-2xl">
+                MovieDB
             </Link>
 
-            <div className='text-xl'>
-                <Link to={ROUTES.HOME} className='px-3'>Home</Link>
-                <Link to={ROUTES.POPULAR} className='px-3'>Popular</Link>
-                <Link to={ROUTES.NOW_PLAYING} className='px-3'>Now Playing</Link>
-                <Link to={ROUTES.TOP_RATED} className='px-3'>Top Rated</Link>
-                <Link to={ROUTES.FAVOURITES} className='px-3'>Favourites</Link>
+            <div className="text-xl">
+                <Link to={ROUTES.HOME} className="px-3">
+                    Home
+                </Link>
+                <Link to={ROUTES.POPULAR} className="px-3">
+                    Popular
+                </Link>
+                <Link to={ROUTES.NOW_PLAYING} className="px-3">
+                    Now Playing
+                </Link>
+                <Link to={ROUTES.TOP_RATED} className="px-3">
+                    Top Rated
+                </Link>
+                <Link to={ROUTES.FAVOURITES} className="px-3">
+                    Favourites
+                </Link>
             </div>
-
         </div>
-    )
-}
+    );
+};
 
-export default TopBar
+export default TopBar;
